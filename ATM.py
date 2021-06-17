564402922690edde0cd11353693f0e683dd714fe
jinal360 <73110780+jinal360@users.noreply.github.com>
2021-06-17 19:06:37 +0530

    Add files via upload

---
+++ ATM.py
@@ -0,0 +1,13 @@
+class Atm:
+    def __init__(self,cardNumber,pin):
+        self.cardNumber = cardNumber
+        self.pin = pin
+
+        def CashWithdrawl(self):
+            print("You have withdrawl 1000 cash from atm")
+
+            def BalanceEnquiry(self):
+                print("We hope that your BalanceEnquiry was solved")
+
+                def CashDeposit(self):
+                    print("You have deposited 1000 rupees in you bank account")
